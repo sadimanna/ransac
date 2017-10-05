da512622797121568f4c351b5cb5fd399809fe11
Siladittya Manna <sadimanna@users.noreply.github.com>
2017-10-05 10:21:19 +0530

    Update imageStitching.m

--- imageStitching.m
+++ imageStitching.m
@@ -1,6 +1,6 @@
 %% Taking Photographs
-img1 = im2double(rgb2gray(imread('office1.jpg')));
-img2 = im2double(rgb2gray(imread('office2.jpg')));
+img1 = im2double(rgb2gray(imread(path_to_image1)));
+img2 = im2double(rgb2gray(imread(path_to_image2)));
 
 %% Obtaininig Correspondences
 points1 = detectSURFFeatures(img1);
@@ -21,22 +21,19 @@ img2_points = matchedPoints2.Location;
 
 A_ransac = estimateTransformRANSAC(img1_points,img2_points)
 
-%% Applying Homography
-% tform = projective2d(inv(A)')
-% transformedImage = imwarp(img2,tform);
-% imshow(transformedImage)
-%% Applying directly to RGB Image
-img2rgb = im2double(imread('office2.jpg'));
+%% Applying Homography Transformation Matrix directly to RGB Image
+img2rgb = im2double(imread(path_to_image2));
 im2_transformed = transformImage(img2rgb,A_ransac);
-%imshow(im2_transformedo)
+%imshow(im2_transformed)
+
 %% Expanding the images
 img1rgb = im2double(imread('office1.jpg'));
 im1_size = size(img1rgb);
 im2_transformed_size = size(im2_transformed);
 padsize = im2_transformed_size-im1_size;
 img1_expanded = padarray(img1rgb,padsize,'post');
-figure
-imshow(img1_expanded)
+%figure
+%imshow(img1_expanded)
 
 %% Blending images
 [x_overlap,y_overlap]=ginput(2);
@@ -46,12 +43,12 @@ diff = overlapright-overlapleft;
 ramp = [zeros(1,overlapleft-1),0:1/diff:1,ones(1,im2_transformed_size(2)-overlapright)];
 im2_blend = im2_transformed .* repmat(ramp,im2_transformed_size(1),1);
 %figure
-%imshow(im2_blendo)
+%imshow(im2_blend)
 
 ramp_for_im1 = 1 - ramp;
 im1_blend = img1_expanded .* repmat(ramp_for_im1,im2_transformed_size(1),1);
 %figure
-%imshow(im1_blendo)
+%imshow(im1_blend)
 
 %% Creating the panorama
 impanorama = im1_blend+im2_blend;
